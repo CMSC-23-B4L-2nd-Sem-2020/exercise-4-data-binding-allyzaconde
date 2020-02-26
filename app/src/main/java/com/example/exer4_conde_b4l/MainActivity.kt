2b41551c899dfab96e5950f9fe3f4681af8e81f9
Allyza <allyzaconde>
2020-02-26 20:54:48 +0800

    chore: create new file

---
+++ app/src/main/java/com/example/exer4_conde_b4l/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.exer4_conde_b4l
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
