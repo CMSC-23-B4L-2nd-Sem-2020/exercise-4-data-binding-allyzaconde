ab90babd3d98b21ff44eddad88b4664c29e81ce2
Allyza <allyzaconde>
2020-03-03 20:19:10 +0800

    feat: add binding to onCreate

--- app/src/main/java/com/example/exer4_conde_b4l/MainActivity.kt
+++ app/src/main/java/com/example/exer4_conde_b4l/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.exer4_conde_b4l
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.exer4_conde_b4l.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
